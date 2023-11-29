ffe3b9fba08fe905b9ef932e3aef67a34d425551
nirgluzman <nir.gluzman@gmail.com>
2023-11-29 23:13:53 +0100

    add props with children to CourseGoal

--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,9 +1,18 @@
-export default function CourseGoal({title, description}: {title: string; description: string}) {
+import {type PropsWithChildren, type ReactNode} from 'react';
+
+// interface CourseGoalProps {
+// 	title: string;
+// 	children: ReactNode; // children are the JSX elements that are passed between opening and closing tags of a component.
+// }
+
+type CourseGoalProps = PropsWithChildren<{title: string}>;
+
+export default function CourseGoal({title, children}: CourseGoalProps) {
 	return (
 		<article>
 			<div>
 				<h2>{title}</h2>
-				<p>{description}</p>
+				{children}
 			</div>
 			<button>Delete</button>
 		</article>
