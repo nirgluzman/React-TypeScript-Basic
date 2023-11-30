47c52234df1babc4e65acdde9779b706233eb198
nirgluzman <nir.gluzman@gmail.com>
2023-11-30 01:02:20 +0100

    add Delete a CourseGoal

--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -6,17 +6,21 @@ import {type PropsWithChildren} from 'react';
 // 	children: ReactNode; // children are the JSX elements that are passed between opening and closing tags of a component.
 // }
 
-type CourseGoalProps = PropsWithChildren<{title: string}>;
+type CourseGoalProps = PropsWithChildren<{
+	id: number;
+	title: string;
+	onDelete: (id: number) => void;
+}>;
 
 // *** regular function syntax ***
-export default function CourseGoal({title, children}: CourseGoalProps) {
+export default function CourseGoal({id, title, onDelete, children}: CourseGoalProps) {
 	return (
 		<article>
 			<div>
 				<h2>{title}</h2>
 				{children}
 			</div>
-			<button>Delete</button>
+			<button onClick={() => onDelete(id)}>Delete</button>
 		</article>
 	);
 }
